f7f15f6c71b12eb2205665a49d322203b927e778
Rossella Carletti <carletti.rossella@gmail.com>
2017-03-02 15:38:16 +0100

    add data package

--- main.go
+++ main.go
@@ -1,100 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"os/user"
-	"path/filepath"
 	"strings"
 	"time"
 
 	owm "github.com/briandowns/openweathermap"
 
+	"encoding/json"
+
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/gmail/v1"
 )
 
-// getClient uses a Context and Config to retrieve a Token
-// then generate a Client. It returns the generated Client.
-func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	cacheFile, err := tokenCacheFile()
-	if err != nil {
-		log.Fatalf("Unable to get path to cached credential file. %v", err)
-	}
-	tok, err := tokenFromFile(cacheFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(cacheFile, tok)
-	}
-	return config.Client(ctx, tok)
-}
-
-// getTokenFromWeb uses Config to request a Token.
-// It returns the retrieved Token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
-}
-
-// tokenCacheFile generates credential file path/filename.
-// It returns the generated credential path/filename.
-func tokenCacheFile() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
-	os.MkdirAll(tokenCacheDir, 0700)
-	return filepath.Join(tokenCacheDir,
-		url.QueryEscape("gmail-go-quickstart.json")), err
-}
-
-// tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encountered.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	t := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(t)
-	defer f.Close()
-	return t, err
-}
-
-// saveToken uses a file path to create a file and store the
-// token in it.
-func saveToken(file string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", file)
-	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func Bod(t time.Time) time.Time {
+func bod(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 23, 59, 0, 0, t.Location())
 }
@@ -108,7 +32,7 @@ type clientInfo struct {
 	weather string
 	mailNum int64
 	senders []string
-	events  []string
+	events  []event
 }
 
 func main() {
@@ -128,7 +52,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(ctx, config)
+	client := Data.GetClient(ctx, config)
 
 	//*****************************************************************
 	//PARTE DELLE MAIL
@@ -172,7 +96,7 @@ func main() {
 	}
 
 	//creo un orario con data odierna e ora 23:59
-	tonight := Bod(time.Now()).Format(time.RFC3339)
+	tonight := bod(time.Now()).Format(time.RFC3339)
 	now := time.Now().Format(time.RFC3339)
 
 	println(now)
@@ -198,7 +122,13 @@ func main() {
 			} else {
 				when = i.Start.Date
 			}
-			fmt.Println(i.Summary, "ore", when[11:13], ":", when[14:16])
+			var e event
+			e.name = i.Summary
+			e.time = when
+
+			c.events = append(c.events, e)
+
+			//fmt.Println(i.Summary, "ore", when[11:13], ":", when[14:16])
 
 		}
 	} else {
@@ -211,6 +141,10 @@ func main() {
 	}
 
 	w.CurrentByName("Pisa")
-	fmt.Println(w.Weather[0])
+	c.weather = w.Weather[0].Description
+	//fmt.Println(w.Weather[0])
+	fo, err := os.Create("output.txt")
+
+	json.NewEncoder(fo).Encode(c)
 
 }
