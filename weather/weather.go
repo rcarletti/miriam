554d210b2300eb4d5ef937af468e0cc42eddde5a
rcarletti <carletti.rossella@gmail.com>
2017-03-19 10:29:26 -0700

    change weather

--- weather/weather.go
+++ weather/weather.go
@@ -13,5 +13,29 @@ func GetWeather(city string) (termperature float64, description string) {
 	}
 	w.CurrentByName(city)
 
-	return w.Main.Temp, w.Weather[0].Description
+	var currW string
+
+	switch w.Weather[0].Icon {
+	case "01d", "01n": //clear sky
+		currW = "01"
+	case "02d", "02n": //few clouds
+		currW = "02"
+	case "03d", "03n": //scattered clouds
+		currW = "03"
+	case "04d", "04n": //few clouds
+		currW = "04"
+	case "09d", "09n": //shower rain
+		currW = "05"
+	case "10d", "10n": // rain
+		currW = "06"
+	case "11d", "11n": //thunderstorm
+		currW = "07"
+	case "13d", "13n": //snow
+		currW = "08"
+	case "50d", "50n": //mist
+		currW = "09"
+
+	}
+
+	return w.Main.Temp, currW
 }
